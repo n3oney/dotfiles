b79b99015be0feee8f787f2ed8439928da24739f
n3oney <neo@neoney.dev>
2023-03-02 23:30:06 +0100

    feat: create initial config

---
+++ hypr/cleanup_after_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 10
+
+hyprctl keyword windowrulev2 "workspace unset,class:discord"
+hyprctl keyword windowrulev2 "workspace unset,class:caprine"
+hyprctl keyword windowrulev2 "workspace unset,class:firefoxdeveloperedition"

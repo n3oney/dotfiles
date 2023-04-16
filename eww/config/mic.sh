75626de5e6d0b5839fcb7d3e870a8da3a9f3634b
n3oney <neo@neoney.dev>
2023-04-17 00:53:04 +0200

    add indicators that show whether my mic is being used or screen recorded

---
+++ eww/config/mic.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+MIC_NAME="Blue Snowball Mono"
+
+source_index=$(pactl -f json list sources | jaq -r ".[] | select( .description == \"$MIC_NAME\" ).index")
+pactl --format json list source-outputs | jaq -r ". | map(select (.source == $source_index and .properties[\"application.name\"] != \"PulseAudio Volume Control\")) | length"
+
+pactl subscribe \
+    | grep --line-buffered -E "'((new)|(remove))' on source-output" \
+    | while read -r evt; do 
+      source_index=$(pactl -f json list sources | jaq -r ".[] | select( .description == \"$MIC_NAME\" ).index")
+
+      pactl --format json list source-outputs | jaq -r ". | map(select (.source == $source_index and .properties[\"application.name\"] != \"PulseAudio Volume Control\")) | length"
+    done

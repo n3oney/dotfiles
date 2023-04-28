8fcdb123f8f6bb94086504b84e4de4acf6b3007f
n3oney <neo@neoney.dev>
2023-04-28 13:34:39 +0200

    add new emojis to fonts file

--- fonts.nix
+++ fonts.nix
@@ -1,48 +1,107 @@
-{vars, ...}:
-with vars; {
-  xdg.configFile."fontconfig/fonts.conf".text = ''
-    <fontconfig>
-     <match target="pattern">
-    	<test name="family" qual="any">
-    		<string>monospace</string>
-    	</test>
-    	<edit binding="strong" mode="prepend" name="family">
-    		<string>${mono_font}</string>
-    	</edit>
-    </match>
+{vars, ...}: let
+  replaceFont = fontName: ''
     <match target="pattern">
-    	<test name="family" qual="any">
-    		<string>sans-serif</string>
-    	</test>
-    	<edit binding="strong" mode="prepend" name="family">
-    		<string>${sans_font}</string>
-    	</edit>
+        <test qual="any" name="family"><string>${fontName}</string></test>
+        <edit name="family" mode="assign" binding="same"><string>JoyPixels</string></edit>
     </match>
-    <match target="pattern">
-    	<test name="family" qual="any">
-    		<string>serif</string>
-    	</test>
-    	<edit binding="strong" mode="prepend" name="family">
-    		<string>${serif_font}</string>
-    	</edit>
-    </match>
-    <match target="font">
-      <edit name="rgba" mode="assign">
-        <const>rgb</const>
-      </edit>
-      <edit name="hinting" mode="assign">
-        <bool>true</bool>
-      </edit>
-      <edit name="hintstyle" mode="assign">
-        <const>hintslight</const>
-      </edit>
-      <edit name="antialias" mode="assign">
-        <bool>true</bool>
-      </edit>
-      <edit name="lcdfilter" mode="assign">
-        <const>lcddefault</const>
-      </edit>
-    </match>
-    </fontconfig>
   '';
-}
+in
+  with vars; {
+    xdg.configFile."fontconfig/fonts.conf".text = ''
+       <fontconfig>
+        <match target="pattern">
+       	<test name="family" qual="any">
+       		<string>monospace</string>
+       	</test>
+       	<edit binding="strong" mode="prepend" name="family">
+       		<string>${mono_font}</string>
+       	</edit>
+       </match>
+       <match target="pattern">
+       	<test name="family" qual="any">
+       		<string>sans-serif</string>
+       	</test>
+       	<edit binding="strong" mode="prepend" name="family">
+       		<string>${sans_font}</string>
+       	</edit>
+       </match>
+       <match target="pattern">
+       	<test name="family" qual="any">
+       		<string>serif</string>
+       	</test>
+       	<edit binding="strong" mode="prepend" name="family">
+       		<string>${serif_font}</string>
+       	</edit>
+       </match>
+
+      <match target="pattern">
+          <test qual="any" name="family"><string>emoji</string></test>
+          <edit name="family" mode="assign" binding="same"><string>JoyPixels</string></edit>
+      </match>
+
+
+      <match target="pattern">
+          <test name="family"><string>sans</string></test>
+          <edit name="family" mode="append"><string>JoyPixels</string></edit>
+      </match>
+
+      <match target="pattern">
+          <test name="family"><string>serif</string></test>
+          <edit name="family" mode="append"><string>JoyPixels</string></edit>
+      </match>
+
+      <match target="pattern">
+          <test name="family"><string>sans-serif</string></test>
+          <edit name="family" mode="append"><string>JoyPixels</string></edit>
+      </match>
+
+      <match target="pattern">
+          <test name="family"><string>monospace</string></test>
+          <edit name="family" mode="append"><string>JoyPixels</string></edit>
+      </match>
+
+       <selectfont>
+           <rejectfont>
+                 <pattern>
+                     <patelt name="family">
+                         <string>Symbola</string>
+                     </patelt>
+                 </pattern>
+             </rejectfont>
+       </selectfont>
+
+       ${toString (map (x: replaceFont x) [
+        "Apple Color Emoji"
+        "Segoe UI Emoji"
+        "Segoe UI Symbol"
+        "Android Emoji"
+        "Twitter Color Emoji"
+        "Twemoji"
+        "Twemoji Mozilla"
+        "TwemojiMozilla"
+        "Emoji Two"
+        "EmojiTwo"
+        "EmojiSymbols"
+        "Symbola"
+      ])}
+
+       <match target="font">
+         <edit name="rgba" mode="assign">
+           <const>rgb</const>
+         </edit>
+         <edit name="hinting" mode="assign">
+           <bool>true</bool>
+         </edit>
+         <edit name="hintstyle" mode="assign">
+           <const>hintslight</const>
+         </edit>
+         <edit name="antialias" mode="assign">
+           <bool>true</bool>
+         </edit>
+         <edit name="lcdfilter" mode="assign">
+           <const>lcddefault</const>
+         </edit>
+       </match>
+       </fontconfig>
+    '';
+  }
